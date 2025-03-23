4bac2c403688ddaa5c38b8425535ed76df01f27e
Ed Cabiltes <edcabiltes@outlook.com>
2025-03-23 16:25:17 +0800

    model movement and delivery

--- src/models/villageState.js
+++ src/models/villageState.js
@@ -3,4 +3,35 @@ import { roads } from "../constants/roads.js";
 
 const roadGraph = buildGraph(roads);
 
-console.log(roadGraph);
+export class VillageState {
+    constructor(place, parcels) {
+        this.place = place;
+        this.parcels = parcels;
+    }
+
+    move(destination) {
+        if (!roadGraph[this.place].includes(destination)) {
+            return this;
+        } else {
+            let parcels = this.parcels.map(p => {
+                if (p.place != this.place) return p;
+                return { place: destination, address: p.address };
+            }).filter(p => p.place != p.address);
+            return new VillageState(destination, parcels);
+        }
+    }
+};
+
+let first = new VillageState(
+    "Post Office",
+    [{ place: "Post Office", address: "Alice's House" }]
+);
+
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office
